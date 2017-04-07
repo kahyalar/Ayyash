6e320854ea8060767a60995dff97acf0677870e4
kahyalar <furkan.kahyalar@gmail.com>
2017-04-07 14:59:05 +0300

    Key events added

--- app/src/main/java/com/example/android/ayyash/MainActivity.java
+++ app/src/main/java/com/example/android/ayyash/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.ayyash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -23,19 +25,79 @@ public class MainActivity extends AppCompatActivity {
 
     public void initHelpers()
     {
+        buttonCall.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+
+        buttonMessage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonCamera.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonTaxi.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonExit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Lockscreen.getInstance(MainActivity.this).stopLockscreenService();
+                finish();
+            }
+        });
     }
 
-    public void onBackPressed()
+    public boolean onKeyDown(int keyCode, KeyEvent event)
     {
-        Toast.makeText(this, "You can't close this application via back button. If you want to close it, Press 'Exit Button'. ", Toast.LENGTH_LONG).show();
+        switch (keyCode){
+            case KeyEvent.KEYCODE_HOME:
+                Toast.makeText(MainActivity.this, "You are drunk!", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_BACK:
+                Toast.makeText(MainActivity.this, "You can't close this application via back button. If you want to close it, Press 'Exit Button'. ", Toast.LENGTH_LONG).show();
+                return true;
+            case KeyEvent.KEYCODE_MENU:
+                Toast.makeText(MainActivity.this, "You can't close this application via back button. If you want to close it, Press 'Exit Button'. ", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return true;
+        }
     }
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Lockscreen.getInstance(this).startLockscreenService();
+        Lockscreen.getInstance(MainActivity.this).startLockscreenService();
         initViews();
+        initHelpers();
     }
 }
