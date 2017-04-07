795ee6c5ac8c773c6a7c6ae802423ccdf678d470
kahyalar <furkan.kahyalar@gmail.com>
2017-04-07 14:39:20 +0300

    basic init and structure

--- app/src/main/java/com/example/android/ayyash/MainActivity.java
+++ app/src/main/java/com/example/android/ayyash/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.android.ayyash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.dubu.lockscreenusingservice.Lockscreen;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button buttonCall, buttonMessage, buttonCamera, buttonTaxi, buttonSettings, buttonExit;
+
+    public void initViews()
+    {
+        buttonCall = (Button)findViewById(R.id.btnAra);
+        buttonMessage = (Button)findViewById(R.id.btnMesajGonder);
+        buttonCamera = (Button)findViewById(R.id.btnKamera);
+        buttonTaxi = (Button)findViewById(R.id.btnTaksiCagir);
+        buttonSettings = (Button)findViewById(R.id.btnAyarlar);
+        buttonExit = (Button)findViewById(R.id.btnCikis);
+    }
+
+    public void initHelpers()
+    {
+
+    }
+
+    public void onBackPressed()
+    {
+        Toast.makeText(this, "You can't close this application via back button. If you want to close it, Press 'Exit Button'. ", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lockscreen.getInstance(this).startLockscreenService();
+        initViews();
     }
 }
